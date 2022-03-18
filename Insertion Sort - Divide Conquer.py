f39aa6933c39aed680bd5a8fb006517fe40ef469
Alif Ranadian Nadhifah <45065130+alifnadhifah@users.noreply.github.com>
2022-03-18 10:41:46 +0700

    Update Insertion Sort - Divide Conquer.py

--- Insertion Sort - Divide Conquer.py
+++ Insertion Sort - Divide Conquer.py
@@ -5,39 +5,55 @@ Created on Wed Apr 15 19:32:56 2020
 @author: Alifskiy
 """
 
-def insertionSort(list, n):
-    j=0
+#function insertion sort algorithm ascending
+def insertionSort1(list, n):
     print("")
-    print("Output Array: ", list[0:1],"(iterasi ke-",j,")")
+    print("Output Array: ", list, '(iterasi ke-0)')
+    #Algoritma Utama Insertion Sort ascending
     for i in range(1,n):
         value = list[i]
-        i -= 1
-        while i >= 0 :
-            if value < list[i]:
-                list[i+1] = list[i]
-                list[i] = value
-                i -= 1
-                j+=1
-                k = j + 1
-                print("Output Array: ", list[0:k],"(iterasi ke-",j,")")
-            else:
-                value = list[i]
-                i -= 1
-                j+=1
-                m = j + 1
-                print("Output Array: ", list[0:m],"(iterasi ke-",j,")")
+        j = i - 1
+        #Algoritma Utama Divide & Conquer
+        while j>= 0 and list[j] > value:
+            list[j+1] = list[j]
+            j -= 1
+        list[j+1] = value
+        print("Output Array: ", list, '(iterasi ke-',i,')')
 
+def insertionSort2(list, n):
+    print("")
+    print("Output Array: ", list, '(iterasi ke-0)')
+    #Algoritma Utama Insertion Sort descending
+    for i in range(1,n):
+        value = list[i]
+        j = i - 1
+        #Algoritma Utama Divide & Conquer
+        while j>= 0 and list[j] < value:
+            list[j+1] = list[j]
+            j -= 1
+        list[j+1] = value
+        print("Output Array: ", list, '(iterasi ke-',i,')')
+
+#start program with initialization and variable assignment
 arr = []
 
 k = int(input('Panjang Array: '))
 
 for l in range(k):
-    x = int(input('Isi Array: '))
+    x = int(input(f'Isi Array ke-{l}: '))
     arr.append(x)
-    print("Array: ", arr)
 
+#print variable value
 print('')
 print("Output Array: ", arr,"(Kondisi Awal Array)")
+print('')
 print('Hasil Divide Conquer: ')
-insertionSort(arr, k)
-
+ascordesc = str(input('Urutkan Secara Ascending/Descending(asc/desc): '))
+if (ascordesc=='asc'):
+    print('Hasil Divide Conquer: ')
+    insertionSort1(arr, k)
+    print('Pengurutan secara Ascending Selesai :)')
+elif (ascordesc=='desc'):
+    print('Hasil Divide Conquer: ')
+    insertionSort2(arr, k)
+    print('Pengurutan secara Descending Selesai :)')
